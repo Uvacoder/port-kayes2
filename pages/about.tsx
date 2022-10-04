12d08645ec9df23587bff087fa51aba326a2cb76
its-kayes <kayes.ek8@gmail.com>
2022-10-04 23:55:00 +0600

    Home Banner Create

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,25 @@
+type user = {
+    name: string,
+    age: number,
+    email: string,
+    number: number,
+    address: string
+}
+
 export default function about() {
+
+    const userData: user = {
+        name: "kayes",
+        age: 22,
+        email: 'kayes.ek8@gmail.com',
+        number: 9227220,
+        address: "Uttara, Dhaka"
+    }
+
     return (
-        <div>
-            about file
+        <div className="text-white">
+            about file {userData.name || userData.age}
+
         </div>
     )
 }
